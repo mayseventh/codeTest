a029fa972c15c746a289d0538072a68365148ec2
Taesun.eom <ets0057@gmail.com>
2023-02-27 17:46:15 +0900

    230227_update

---
+++ 안전지대_update.py
@@ -0,0 +1,18 @@
+from pprint import pprint
+
+def solution(board):
+    answer = 0
+    
+    answer = len(board) * len(board[0])
+    print(answer)
+
+    return answer
+
+#1
+print(solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]))
+#2
+# print(solution([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 1, 0], [0, 0, 0, 0, 0]]))
+# #3
+# print(solution([[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]))
+# 4
+# print(solution([[0]]))
